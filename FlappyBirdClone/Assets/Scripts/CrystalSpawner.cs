c25ce7963ce489af9be373c5b9d2df5dcf22c507
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-05-30 23:54:54 +0300

    Added Project Unity Files

---
+++ FlappyBirdClone/Assets/Scripts/CrystalSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpawner : MonoBehaviour
+{
+    public GameObject crystalPrefab; // Prefab of the crystal to spawn
+    public float spawnRate = 1.5f; // Difficulty multiplier
+    private float timer = 0;
+    private float highestY = 0.62f; // Initial highest Y position
+    private float lowestY = -0.62f; // Initial highest Y position
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnObject(); // Spawn the first crystal immediately
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= spawnRate)    // u cannot write == because this is a float decimal time.deltatime and will neveer be equal to exactly 2
+        {
+            // Instantiate a crystal at a random height within the specified range
+            Instantiate(crystalPrefab, new Vector3(transform.position.x,Random.Range(highestY, lowestY), -1), transform.rotation);
+            timer = 0;
+        }
+        else
+            timer += Time.deltaTime;
+
+    }
+
+    void spawnObject()
+    {
+        Instantiate(crystalPrefab, new Vector3(transform.position.x, Random.Range(highestY, lowestY), -1), transform.rotation);
+    }
+}
---
+++ FlappyBirdClone/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // to fetch rigidbody2d object
+    public Rigidbody2D rb;
+    public LogicHandler logic;
+    public Animator animator;
+    public AudioSource boom;
+    public AudioSource tick;
+    public AudioSource backgroundmusic;
+    public float jumpSpeed = 1.5f;
+    public bool alive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.Find("Logic Handler").GetComponent<LogicHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) && (alive))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("CheckPoint") && alive)
+        {
+            tick.Play();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeadZone") && alive)
+        {
+            backgroundmusic.Stop();
+            boom.Play();
+            animator.Play("Player_Dead");
+            alive = false;
+            logic.GameOver();
+        }
+    }
+}
